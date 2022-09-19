e2c642f285315bf835a98ab443b8d6e99d0e5894
Nikki Rufiansya <nikkirufiansya88@gmail.com>
2022-09-19 18:52:03 +0700

    update forgot password
    fixing code in forgot password

--- lib/auth/forgot_password.dart
+++ lib/auth/forgot_password.dart
@@ -1,22 +1,26 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(ForgotPassword());
-}
 
 class ForgotPassword extends StatelessWidget {
   const ForgotPassword({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Icon(
-          Icons.arrow_back_ios,
-          size: 20,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Icon(
+            Icons.arrow_back,
+            color: Colors.grey,
+          ),
+          elevation: 0,
+          backgroundColor: Colors.white,
         ),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+            child: Column(
+          children: [],
+        )),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:kabar/auth/forgot_password.dart';
 import 'package:kabar/auth/login.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool _isObscure = true;
-  bool isChecked = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return LoginApp();
-  }
+  runApp(const LoginApp());
 }
